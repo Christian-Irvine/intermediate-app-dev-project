7dbb9f33b50c79eef573eee3cdeaa788a982f415
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-04-01 10:32:23 +1300

    feat(movie-listings): add date styling on card

--- movie-listings-application/src/components/MovieCard.tsx
+++ movie-listings-application/src/components/MovieCard.tsx
@@ -19,13 +19,13 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = (props: MovieCardProps) => {
   return (
     <>
-      <Card className="bg-cover aspect-[2/3] py-0 flex shadow-md" style={{ backgroundImage: `url("${`https://image.tmdb.org/t/p/w500${props.posterPath}`}")` }}>
+      <Card className="bg-cover aspect-[2/3] py-0 flex shadow-md justify-between" style={{ backgroundImage: `url("${`https://image.tmdb.org/t/p/w500${props.posterPath}`}")` }}>
         <CardTitle className="text-white">
-          <div className="bg-linear-to-t from-0% to-gray-950 py-3 rounded-xl">{props.name}</div>
+          <div className="bg-linear-to-t from-0% to-gray-950 py-3 rounded-t-xl">{props.name}</div>
         </CardTitle>
-        <CardFooter className="text-white">
-          <div className="bg-linear-to-b from-0% to-gray-950 py-2">{props.releaseDate}</div>
-        </CardFooter>
+        <CardTitle className="text-white">
+          <div className="bg-linear-to-b from-0% to-gray-950 py-2 rounded-b-xl">{props.releaseDate}</div>
+        </CardTitle>
       </Card>
     </>
   )
