b8ef3f8e4f3e9bf036a3e90b8de0ee0cd1b4a0de
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-04-01 09:04:34 +1300

    feat(movie-listings): add overview and releaseDate to movieCard props

--- movie-listings-application/src/components/MovieCard.tsx
+++ movie-listings-application/src/components/MovieCard.tsx
@@ -12,19 +12,17 @@ import '../App.css'
 interface MovieCardProps {
   name: string,
   posterPath: string,
-  popularity: number,
+  overview: string,
+  releaseDate: string,
 }
 
 const MovieCard: React.FC<MovieCardProps> = (props: MovieCardProps) => {
   return (
     <>
       <Card className="bg-cover aspect-[2/3] py-0 flex justify-end shadow-md" style={{ backgroundImage: `url("${`https://image.tmdb.org/t/p/w500${props.posterPath}`}")` }}>
-          <CardTitle className="text-white ">
+          <CardTitle className="text-white">
             <div className="bg-linear-to-b from-0% to-gray-950 py-2">{props.name}</div>
           </CardTitle>
-        {/* <CardContent>
-          <p>Popularity: {props.popularity}</p>
-        </CardContent> */}
       </Card>
     </>
   )
