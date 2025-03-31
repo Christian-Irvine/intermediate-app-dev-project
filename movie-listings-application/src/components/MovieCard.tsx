44f99d90a0488b52a60c65e956e5ad72179bbbc1
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-04-01 10:50:08 +1300

    feat(movie-listings): add description of movies to card

--- movie-listings-application/src/components/MovieCard.tsx
+++ movie-listings-application/src/components/MovieCard.tsx
@@ -1,9 +1,5 @@
 import {
   Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
   CardTitle,
 } from "./ui/card"
 
@@ -19,14 +15,19 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = (props: MovieCardProps) => {
   return (
     <>
-      <Card className="bg-cover aspect-[2/3] py-0 flex shadow-md justify-between" style={{ backgroundImage: `url("${`https://image.tmdb.org/t/p/w500${props.posterPath}`}")` }}>
-        <CardTitle className="text-white">
-          <div className="bg-linear-to-t from-0% to-gray-950 py-3 rounded-t-xl">{props.name}</div>
-        </CardTitle>
-        <CardTitle className="text-white">
-          <div className="bg-linear-to-b from-0% to-gray-950 py-2 rounded-b-xl">{props.releaseDate}</div>
-        </CardTitle>
-      </Card>
+      <div className="flex flex-col">
+        <Card className="bg-cover aspect-[2/3] py-0 flex shadow-md justify-between" style={{ backgroundImage: `url("${`https://image.tmdb.org/t/p/w500${props.posterPath}`}")` }}>
+          <CardTitle className="text-white">
+            <div className="bg-linear-to-t from-0% to-gray-950 py-3 rounded-t-xl">{props.name}</div>
+          </CardTitle>
+          <CardTitle className="text-white">
+            <div className="bg-linear-to-b from-0% to-gray-950 py-2 rounded-b-xl">{props.releaseDate}</div>
+          </CardTitle>
+        </Card>
+        <div className="mx-3 px-2 bg-slate-200 rounded-b-xl aspect-[4/3] shadow-md">
+          <p className="font-medium">{props.overview}</p>
+        </div>
+      </div>
     </>
   )
 }
