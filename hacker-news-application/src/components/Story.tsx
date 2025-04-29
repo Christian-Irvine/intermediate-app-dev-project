e1cfdbc180c20f4c970cd79367f95c113d960ff1
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-04-30 10:50:14 +1200

    feat(hacker-news): add styling to story page

--- hacker-news-application/src/components/Story.tsx
+++ hacker-news-application/src/components/Story.tsx
@@ -1,15 +1,38 @@
 import { useParams } from "react-router";
+import { getStoryApiRoute } from "../Utils";
+import { useQuery } from "@tanstack/react-query";
+import parse from 'html-react-parser';
 
 import "../App.css";
 
 const Story: React.FC = () => {
   const { id } = useParams();
 
-
+  const {
+    isLoading,
+    error,
+    data: storyData,
+  } = useQuery({
+    queryKey: [id],
+    queryFn: () =>
+      fetch(getStoryApiRoute(id || "")).then((res: Response) => res.json()),
+  });
   
+  if (isLoading) return <h1 className="p-20">Loading...</h1>;
+  if (error) 
+    return (
+      <>
+        <h2 className="p-20">Something went wrong, please try again later.</h2>
+        <p>{error.message}</p> 
+      </>
+    );
+
   return (
     <>
-      <p>Hello! this is {id}</p>
+    <section className="mx-100 px-20 justify-start text-left bg-slate-100">
+      <h2 className="font-bold py-10">{storyData.title}</h2>
+      <p className="text-lg">{parse(storyData.text || "No description.")}</p>
+    </section>
     </>
   );
 };
