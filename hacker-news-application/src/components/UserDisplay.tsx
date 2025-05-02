a2c2ef869168f535c0675b9824129f0fcf1dbd6b
Christian Irvine <christian.irvine04@gmail.com>
2025-05-02 22:13:45 +1200

    feat: add user searching and data display in card

---
+++ hacker-news-application/src/components/UserDisplay.tsx
@@ -0,0 +1,69 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+} from "./ui/card"
+import parse from 'html-react-parser';
+import { formatTime } from "../Utils";
+
+export interface UserInfo {
+  name: string;
+}
+
+const UserDisplay: React.FC<UserInfo> = (props: UserInfo) => {
+  const {
+    isLoading,
+    error,
+    data: userData,
+  } = useQuery({
+    queryKey: [props.name],
+    queryFn: () =>
+      fetch(getApiRoute(props.name)).then((res: Response) => res.json()),
+  });
+
+  const getApiRoute = (name: string) => {
+    return `https://hacker-news.firebaseio.com/v0/user/${name}.json?print=pretty`
+  }
+
+  if (isLoading) return <h1 className="p-20">Loading...</h1>;
+  if (error) 
+    return (
+      <>
+        <h2 className="p-20">Something went wrong, please try again later.</h2>
+        <p>{error.message}</p> 
+      </>
+    );
+
+  const maxSubmitted: number = 5;
+
+  const storyMax: number = Math.min(maxSubmitted, userData.submitted.length);
+  const submittedData: Array<Number> = userData.submitted.slice(0, storyMax);
+
+  return (
+    <>
+      <Card className="bg-cover aspect-[15/16] py-0 flex shadow-md justify-between bg-slate-700">
+        <CardTitle className="text-white font-bold text-lg">
+          <p className="bg-linear-to-t from-0% to-gray-950 py-5 rounded-t-xl px-5">{userData.id || "No Id."}</p>
+        </CardTitle>
+        <CardDescription className="text-lg text-slate-300 p-5">
+          {parse(userData.about || "No description.")}
+        </CardDescription>
+        <CardDescription className="text-lg text-slate-300 p-5">
+          Karma: {userData.karma || 0}
+        </CardDescription>
+        <CardDescription className="text-lg text-slate-300 p-5">
+          {submittedData.map((submitted: Number) => (
+            <a key={submitted.toString()} className="text-lg pt-5" href={`https://hacker-news.firebaseio.com/v0/item/${submitted}.json?print=pretty`} target="_blank"><p>{`https://hacker-news.firebaseio.com/v0/item/${submitted}.json?print=pretty`}</p></a>           
+          ))}
+        </CardDescription>
+        <CardTitle className="text-white">
+          <p className="bg-linear-to-b from-0% to-gray-950 py-5 rounded-b-xl px-5">{`Account created on: ${formatTime(userData.created || 0)}`}</p>
+        </CardTitle>
+      </Card>
+    </>
+  )
+}
+  
+export default UserDisplay
+  
--- hacker-news-application/src/components/UserSearch.tsx
+++ hacker-news-application/src/components/UserSearch.tsx
@@ -1,35 +1,32 @@
 import { useForm } from "react-hook-form"
+import UserDisplay from "./UserDisplay";
+import { UserInfo } from "./UserDisplay";
+import { useState } from "react";
 
 const UserSearch: React.FC = () => {
   const userForm = useForm();
+  const users: Array<string> = ['tptacek', 'jacquesm', 'ingve', 'todsacerdoti', 'rbanffy', 'pseudolus', 'danso', 'tosh', 'JumpCrisscross', 'Tomte'];
 
-  const users = ['tptacek', 'jacquesm', 'ingve', 'todsacerdoti', 'rbanffy', 'pseudolus', 'danso', 'tosh', 'JumpCrisscross', 'Tomte', 'Animats', 'lxm', 'ColinWright', 'patio11', 'zdw', 'rayiner', 
-    'dragonwriter', 'pjmlp', 'luu', 'TeMPOraL', 'ChuckMcM', 'thunderbong', 'pjc50', 'toomuchtodo', 'anigbrowl', 'steveklabnik', 'jgrahamc', 'PaulHoule', 'bookofjoe', 'walterbell', 'coldtea', 'jerf', 'userbinator',
-    'signa11', 'ceejayoz', 'nostrademons', 'mooreds', 'sohkamyung', 'uptown', 'WalterBright', 'jedberg', 'crazygringo', 'jrockway', 'hn_throwaway_99', 'simonw', 'paxys', 'stavros', 'minimaxir', 'mfiguiere', 'doener',
-    'wglb', 'shawndumas', 'masklinn', 'dnetesn', 'davidw', 'coloneltcb', 'robin_reala', 'jonbaer', 'Brajeshwar', 'dredmorbius', 'aaronbrethorst', 'cperciva', 'tokenadult', 'rntn', 'tyingq', 
-    'belter', 'wpietri', 'saagarjha', 'fogus', 'Retric', 'adamnemecek', 'Bender', 'wallflower', 'evo_9', 'lisper', 'ryandrake', 'sp332', 'bane', 'derefr', 'btilly', 'mpweiher', 'nkurz',
-    'Anon84', 'kibwen', 'brudgers', 'ilamont', 'bpierre', 'pavel_lishin', 'petercooper', 'pavlov', 'Someone1234', 'ksec', 'tzs', 'wmf', 'pabs3', 'nostromo', 'mmastrac', 'onion2k', 'DiabloD3', 'feross']
+  const [selectedUsername, setSelectedUsername] = useState<string>('');
 
-  const handleInstitutionSubmit = (values: Object) => {
-    console.log(values);
+  const handleUserSubmit = (values: UserInfo) => {
+    setSelectedUsername(values.name);
+    console.log(selectedUsername);
   }
 
   return (
     <>
       <section className="mx-100 px-20 bg-slate-100 justify-start text-left">
-        <form className="py-20" onSubmit={userForm.handleSubmit(handleInstitutionSubmit)}>
-          <table>
-            <tr>
-              <label className="font-bold" htmlFor="name">Search for user</label>
-            </tr>
-            <tr>
-              <input className="border-solid" type="text" id="name" {...userForm.register("name")} />
-            </tr>
-            <tr>
-              <button className="bg-slate-100" type="submit">Submit</button>
-            </tr>
-          </table>
+        <form className="py-20" onSubmit={userForm.handleSubmit(handleUserSubmit)}>
+          <label className="font-bold" htmlFor="name">Search for user</label>
+          <input className="border-solid" type="text" id="name" {...userForm.register("name")} />
+          <button className="bg-slate-100" type="submit">Submit</button>
         </form>
+        {users.includes(selectedUsername) ? (
+          <UserDisplay name={selectedUsername}/>
+        ) : selectedUsername !== '' && 
+          <p>Couldn't find user {selectedUsername}.</p>
+        }
       </section>
     </>
   );
