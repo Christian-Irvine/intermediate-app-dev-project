9a746356732644a8892d71c46f8c87d22044fc41
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-05-20 09:51:30 +1200

    feat: add styling to quiz form

--- trivia-application/src/components/QuizSelection.tsx
+++ trivia-application/src/components/QuizSelection.tsx
@@ -62,39 +62,53 @@ const QuizSelection: React.FC<QuizSelectionProps> = (props: QuizSelectionProps) 
 
   return (
     <>
-      <form onSubmit={quizSelectionForm.handleSubmit(() => props.handleFormSubmit())}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" defaultValue={props.defaultFormValues.name} {...quizSelectionForm.register("name")} />
+      <section>
+        <h2 className="font-bold text-2xl justify-self-center pb-10 pt-5">Craft Your Quiz!</h2>
 
-        <label htmlFor="amount">Amount</label>
-        <input type="number" id="amount" defaultValue={props.defaultFormValues.amount} {...quizSelectionForm.register("amount")} />
+        <form onSubmit={quizSelectionForm.handleSubmit(() => props.handleFormSubmit())} className="justify-center flex">
+          <article className="px-5">
+            <label htmlFor="name" className="pr-2">Name:</label>
+            <input type="text" id="name" defaultValue={props.defaultFormValues.name} {...quizSelectionForm.register("name")} />
+          </article>
+          
+          <article className="px-5">
+            <label htmlFor="amount" className="pr-2">Amount:</label>
+            <input type="number" id="amount" defaultValue={props.defaultFormValues.amount} {...quizSelectionForm.register("amount")} />
+          </article>
 
-        <label htmlFor="category">Category</label>
-        <select id="category" defaultValue={props.defaultFormValues.category} {...quizSelectionForm.register("category")}>
-          <option value={props.defaultFormValues.category}>{getDisplayName(props.defaultFormValues.category)}</option>
-          {categoryData.trivia_categories.map((category: any) => (
-            <option key={category.id} value={category.id}>{getDisplayName(category.name)}</option>
-          ))}
-        </select>
+          <article className="px-5">
+            <label htmlFor="category" className="pr-2">Category:</label>
+            <select id="category" defaultValue={props.defaultFormValues.category} {...quizSelectionForm.register("category")}>
+              <option value={props.defaultFormValues.category}>{getDisplayName(props.defaultFormValues.category)}</option>
+              {categoryData.trivia_categories.map((category: any) => (
+                <option key={category.id} value={category.id}>{getDisplayName(category.name)}</option>
+              ))}
+            </select>
+          </article>
 
-        <label htmlFor="difficulty">Difficulty</label>
-        <select id="difficulty" defaultValue={props.defaultFormValues.difficulty} {...quizSelectionForm.register("difficulty")}>
-          <option value={props.defaultFormValues.difficulty}>{getDisplayName(props.defaultFormValues.difficulty)}</option>
-          {difficulties.map((difficulty: any) => (
-            <option key={difficulty} value={difficulty}>{getDisplayName(difficulty)}</option>
-          ))}
-        </select>
+          <article className="px-5">
+            <label htmlFor="difficulty" className="pr-2">Difficulty:</label>
+            <select id="difficulty" defaultValue={props.defaultFormValues.difficulty} {...quizSelectionForm.register("difficulty")}>
+              <option value={props.defaultFormValues.difficulty}>{getDisplayName(props.defaultFormValues.difficulty)}</option>
+              {difficulties.map((difficulty: any) => (
+                <option key={difficulty} value={difficulty}>{getDisplayName(difficulty)}</option>
+              ))}
+            </select>
+          </article>
 
-        <label htmlFor="type">Type</label>
-        <select id="type" defaultValue={props.defaultFormValues.type} {...quizSelectionForm.register("type")}>
-          <option value={props.defaultFormValues.type}>{getDisplayName(props.defaultFormValues.type)}</option>
-          {types.map((type: any) => (
-            <option key={type.id} value={type.id}>{type.name}</option>
-          ))}
-        </select>
-        
-        <button type="submit">Submit</button>
-      </form>
+          <article className="px-5">
+            <label htmlFor="type" className="pr-2">Type:</label>
+            <select id="type" defaultValue={props.defaultFormValues.type} {...quizSelectionForm.register("type")}>
+              <option value={props.defaultFormValues.type}>{getDisplayName(props.defaultFormValues.type)}</option>
+              {types.map((type: any) => (
+                <option key={type.id} value={type.id}>{type.name}</option>
+              ))}
+            </select>
+          </article>
+          
+          <button type="submit">Submit</button>
+        </form>
+      </section>
     </>
   )
 }
