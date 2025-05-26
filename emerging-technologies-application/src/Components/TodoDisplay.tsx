8712e94e1004c60d2c35db0736e964aa749f2fdc
Christian Irvine (1000117593) <IRVICJ5@student.op.ac.nz>
2025-05-27 11:57:53 +1200

    feat(emerging-techno): add X button to remove todo

--- emerging-technologies-application/src/Components/TodoDisplay.tsx
+++ emerging-technologies-application/src/Components/TodoDisplay.tsx
@@ -4,10 +4,13 @@ import { Todo } from './TodoList';
 
 const TodoDisplay: Component<Todo> = (props) => {
   return (
-    <article className="bg-orange-100 ratio aspect-square rounded-3xl p-10 border-orange-200 border-solid border-4 shadow-2xl">
+    <article className="bg-orange-100 aspect-square rounded-3xl p-10 border-orange-200 border-solid border-4 shadow-2xl relative">
       <h2 className="font-bold text-2xl text-center pb-5">{props.name === '' || !props.name ? 'Untitled' : props.name}</h2>
       <p>{props.description === '' || !props.description ? 'No Description.' : props.description}</p>
       <p>{props.isComplete.toString()}</p>
+      <button className="absolute -top-5 -right-5 w-15">
+        <div className="bg-orange-100 rounded-full aspect-square border-4 border-orange-200 justify-center flex items-center font-bold text-2xl">✕</div>
+      </button>
     </article>
   );
 };
