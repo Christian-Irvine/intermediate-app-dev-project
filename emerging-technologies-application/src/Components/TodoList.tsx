3bb66cc81bf53cd13dd0550eb3ee5c85e80510dc
Christian Irvine <christian.irvine04@gmail.com>
2025-05-27 18:36:32 +1200

    feat: add filtering based on completion

--- emerging-technologies-application/src/Components/TodoList.tsx
+++ emerging-technologies-application/src/Components/TodoList.tsx
@@ -1,4 +1,5 @@
 import { type Component, createSignal, Index } from 'solid-js';
+import { Show } from "solid-js"
 
 import TodoDisplay from './TodoDisplay';
 
@@ -31,6 +32,7 @@ const TodoList: Component = () => {
   }
   ];
 
+  const [filter, setFilter] = createSignal<string>('');
   const [todos, setTodos] = createSignal<Array<Todo>>(defaultTodos);  
 
   const handleTodoRemove: Function = (index: number) => {
@@ -48,7 +50,6 @@ const TodoList: Component = () => {
     const newTodos: Array<Todo> = [...todos()];
     newTodos[index] = newData;
     setTodos(newTodos);
-    console.log(todos());
   }
 
   const handleTodoAdd: Function = () => {
@@ -56,10 +57,25 @@ const TodoList: Component = () => {
     newTodos.push({
       name: "",
       description: "",
-      isComplete: false,
+      isComplete: filter() === 'complete',
     });
     setTodos(newTodos);
-    console.log(todos());
+  }
+
+  const handleFilterChange: Function = (filterValue) => {
+    setFilter(filterValue);
+  }
+
+  const checkFilter: Function = (isComplete: boolean) => {
+    if (filter() === '') {
+      return true;
+    }
+    else if (filter() === 'complete') {
+      return isComplete;
+    }
+    else {
+      return !isComplete;
+    }
   }
 
   return (
@@ -68,10 +84,17 @@ const TodoList: Component = () => {
       <header class="p-10 shadow-xl sticky bg-orange-300">
         <h1 class="text-7xl text-center font-bold">Todo Time</h1>
       </header>
+        <select name="filter" id="filter" value={filter()} onChange={(value) => handleFilterChange(value.currentTarget.value)} class="mx-35 mt-10 p-3 bg-orange-100 rounded-2xl border-4 border-orange-200">
+          <option value="">No Filter</option>
+          <option value="complete">Completed</option>
+          <option value="not-complete">Not Completed</option>
+        </select>
       <section class="grid grid-cols-3 gap-20 px-35 mt-15 bg-amber-100">
         <Index each={todos()}>
           {(todo, i) =>
-            <TodoDisplay name={todo().name} description={todo().description} isComplete={todo().isComplete} index={i} handleTodoRemove={handleTodoRemove} handleTodoModify={handleTodoModify}/>
+            <Show when={checkFilter(todo().isComplete)}>
+              <TodoDisplay name={todo().name} description={todo().description} isComplete={todo().isComplete} index={i} handleTodoRemove={handleTodoRemove} handleTodoModify={handleTodoModify}/>
+            </Show>
           }
         </Index>
         <button onClick={() => handleTodoAdd()} class="bg-orange-100 rounded-full aspect-square border-4 border-orange-200 m-40 flex justify-center items-center font-bold align-middle text-9xl shadow-2xl">+
